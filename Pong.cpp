2839f433e39126496c0d37b7c5c9556d0027fd40
TanveenBal <tanveenbal@gmail.com>
2024-12-18 02:06:38 -0800

    Fixed CMake and documentation

--- Pong.cpp
+++ Pong.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 #include "Pong.h"
-#include "src/raylib.h"
+#include "raylib.h"
 using namespace std;
 
-
+/**
+ * Initializes the game window, audio, and resources.
+ */
 void initialize_window(){
     InitWindow(WIDTH, HEIGHT, "Pong");
     SetTargetFPS(FPS);
     InitAudioDevice();
-    lost_heart = LoadSound("resources/Lost_Heart.wav");
-    lost_game = LoadSound("resources/Lost_Game.wav");
-    paddle_hard_hit = LoadSound("resources/Paddle_Hard_Hit.wav");
-    paddle_hit = LoadSound("resources/Paddle_Hit.wav");
-    Heart = LoadTexture("resources/Heart.png");
+    // Load sound effects and textures for the game
+    lost_heart = LoadSound("../resources/Lost_Heart.wav");
+    lost_game = LoadSound("../resources/Lost_Game.wav");
+    paddle_hard_hit = LoadSound("../resources/Paddle_Hard_Hit.wav");
+    paddle_hit = LoadSound("../resources/Paddle_Hit.wav");
+    Heart = LoadTexture("../resources/Heart.png");
 }
 
+/**
+ * Closes the game window and unloads all resources.
+ */
 void close_window(){
     UnloadTexture(Heart);
     UnloadSound(paddle_hit);
@@ -25,11 +33,17 @@ void close_window(){
     CloseWindow();
 }
 
+/**
+ * Draws the game scene including the ball, paddles, lives, and score.
+ * 
+ * @param Ball_Color The color of the ball.
+ */
 void draw_game(Color Ball_Color){
     BeginDrawing();
     ClearBackground(Dark_Blue);
-    DrawRectangle(WIDTH / 2, 0, WIDTH/2, HEIGHT, Blue);
-    DrawCircle(WIDTH / 2, HEIGHT / 2, 150, Light_Blue);
+    DrawRectangle(WIDTH / 2, 0, WIDTH/2, HEIGHT, Blue);  // Game field
+    DrawCircle(WIDTH / 2, HEIGHT / 2, 150, Light_Blue);  // Background circle
+    // Draw remaining lives as heart icons
     for (int life = stats.lives; life > 0; life--) {
         float xPos = (float)life * 50 + 520;
         float yPos = 2.5;
@@ -38,101 +52,135 @@ void draw_game(Color Ball_Color){
                        (Rectangle){xPos, yPos, 50.0, 50.0},
                        (Vector2){0, 0}, 0.0, WHITE);
     }
+    // Draw texts: Lives and rally score
     DrawText(TextFormat("Lives Remaining:"), 325, 15, 30, BLACK);
     DrawText(TextFormat("Rally Score: %d", stats.rally), 380, 550, 30, YELLOW);
-    ball.draw(Ball_Color);
-    paddle.draw();
-    cpuPaddle.draw();
-    DrawFPS(925, 0);
+    ball.draw(Ball_Color);  // Draw the ball
+    paddle.draw();          // Draw player paddle
+    cpuPaddle.draw();       // Draw CPU paddle
+    DrawFPS(925, 0);        // Display FPS
     EndDrawing();
 }
 
+/**
+ * Draws the main menu screen.
+ * Listens for button presses to switch to game or settings.
+ */
 void draw_main_menu_screen() {
     BeginDrawing();
     ClearBackground(Dark_Blue);
-    start_button.draw();
-    settings_button.draw();
+    start_button.draw();    // Draw the start button
+    settings_button.draw(); // Draw the settings button
+    // Transition to game or settings screen based on button press
     if (start_button.isPressed()) {main_menu = false; start_button.reset();}
     if (settings_button.isPressed()) {main_menu = false; settings = true; settings_button.reset();}
     EndDrawing();
 }
 
+/**
+ * Draws the settings screen and allows the user to adjust speed settings.
+ */
 void draw_settings_screen(){
     BeginDrawing();
     ClearBackground(Dark_Blue);
-    set_speed_button.draw();
-    speed_box.draw();
-    speed_box.Update();
-    exit_settings.draw();
-    DrawText(TextFormat("Speed Increase Rate = %f", stats.speed), 280, 0, 30, BLACK);
+    set_speed_button.draw();   // Draw the set speed button
+    speed_box.draw();          // Draw input box for speed
+    speed_box.Update();        // Update the speed box input
+    exit_settings.draw();      // Draw the exit settings button
+    std::ostringstream ss;
+    ss << std::fixed << std::setprecision(1) << stats.speed;
+    DrawText(TextFormat("Speed Increase Rate = %s", ss.str().c_str()), 280, 0, 30, BLACK);
+    // Set the speed based on input
     if (set_speed_button.isPressed()) {stats.speed = atof(speed_box.getText()); set_speed_button.reset();}
-    if (exit_settings.isPressed()) {settings = false; main_menu = true; exit_settings.reset();}
+    if (exit_settings.isPressed()) {stats.lives = LIVES; settings = false; main_menu = true; exit_settings.reset();}
     EndDrawing();
 }
 
-void draw_end_screen(){
+/**
+ * Draws the end screen when the game is over and handles replay options.
+ */
+void draw_end_screen() {
     BeginDrawing();
     ClearBackground(Dark_Blue);
     if (play_sound_once) {PlaySound(lost_game); play_sound_once = false;}
-
+    exit_end_screen.draw();   // Draw exit button on the end screen
+    // Reset game when exit button is pressed
+    if (exit_end_screen.isPressed()) {stats.lives = LIVES; settings = false; main_menu = true; exit_end_screen.reset();play_sound_once = true;}
     EndDrawing();
 }
 
+/**
+ * Handles the ball's position, checking for collisions and updating scores.
+ */
 void ball_pos(){
+    // Handle paddle-ball collisions
     if (paddle.collided(ball.position, ball.radius) || cpuPaddle.collided(ball.position, ball.radius)){
         ball.speed_x *= -stats.speed; //increase speed
         ball.speed_y *= stats.speed; //increase speed
         stats.rally += 1;
         if (paddle.collided(ball.position, ball.radius)) {
-            PlaySound(paddle_hard_hit);
+            PlaySound(paddle_hard_hit);  // Play sound if player paddle is hit
         } else {
-            PlaySound(paddle_hit);
+            PlaySound(paddle_hit);  // Play sound if CPU paddle is hit
         }
-    }else if (ball.position.x + ball.radius >= WIDTH - cpuPaddle.size.x + 10 || ball.position.x - ball.radius <= 0 + paddle.size.x + 10) {
+    } 
+    // Handle scoring
+    else if (ball.position.x + ball.radius >= WIDTH - cpuPaddle.size.x + 10 || ball.position.x - ball.radius <= 0 + paddle.size.x + 10) {
         ball.reset((float) WIDTH / 2, (float) HEIGHT / 2, ball.speed_x *= -stats.speed, ball.speed_y *= -stats.speed);
         draw_game(RED);
-        ball.speed_x = 5;
+        ball.speed_x = 5;  // Reset ball speed
         ball.speed_y = 5;
+        // Update player or computer score
         if (ball.position.x + ball.radius >= WIDTH - cpuPaddle.size.x + 10) {stats.player_score += 1;}
         else{stats.computer_score += 1;}
-        PlaySound(lost_heart);
-        WaitTime(.25);
+        PlaySound(lost_heart);  // Play sound on scoring
+        WaitTime(.25);  // Wait after scoring
+        // Track highest rally
         if (stats.rally >= stats.highest_rally) {stats.highest_rally = stats.rally;}
-        stats.lives -= 1;
-        stats.rally = 0;
+        stats.lives -= 1; // Decrease lives
+        stats.rally = 0;  // Reset rally
     }
+    // Handle vertical ball bounce
     if (ball.position.y + ball.radius >= HEIGHT || ball.position.y - ball.radius <= 0) {
         ball.speed_y *= -1;
     }
-    ball.move();
+    ball.move();  // Move the ball
 }
 
+/**
+ * Handles user input to move paddles.
+ */
 void key_inputs(){
     if (IsKeyDown(KEY_W)) paddle.move(-15.0); //move up
-    if (IsKeyDown(KEY_S)) paddle.move(15.0); //move down
-    cpuPaddle.update(ball.position, ball.speed_y);
+    if (IsKeyDown(KEY_S)) paddle.move(15.0);  //move down
+    cpuPaddle.update(ball.position, ball.speed_y); // Update CPU paddle position
 }
 
+/**
+ * Main game loop. Initializes window, handles screen transitions, and updates game state.
+ */
 void run(){
-    initialize_window();
+    initialize_window();  // Initialize window and resources
     while (!WindowShouldClose()) {
         if (main_menu) {
-            draw_main_menu_screen();
+            draw_main_menu_screen();  // Draw main menu
         } else if (settings) {
-            draw_settings_screen();
+            draw_settings_screen();  // Draw settings
         } else if (stats.lives == 0) {
-            draw_end_screen();
+            draw_end_screen();  // Draw end screen if no lives remain
         } else if (!main_menu && !settings){
-            ball_pos();
-            key_inputs();
-            draw_game(Yellow);
+            ball_pos();     // Update ball position
+            key_inputs();   // Handle user input
+            draw_game(Yellow); // Draw the game
         }
-
     }
-    close_window();
+    close_window(); // Close window and resources
 }
 
+/**
+ * Main function to start the game.
+ */
 int main(){
-    run();
+    run(); // Start game
     return 0;
 }
--- Pong.h
+++ Pong.h
@@ -1,55 +1,83 @@
-#include "src/raylib.h"
+#include "raylib.h"
 using namespace std;
 
 #ifndef UNBEATABLE_PONG_PONG_H
-bool main_menu = true;
-bool settings = false;
-bool play_sound_once = true;
-const int WIDTH = 1000;
-const int HEIGHT = 600;
-const int BUTTON_HEIGHT = 50;
-const int BUTTON_WIDTH = 400;
-const int FPS = 60;
-Sound lost_heart;
-Sound lost_game;
-Sound paddle_hard_hit;
-Sound paddle_hit;
-Texture2D Heart;
 
+// Global variables for the game state and resources
+bool main_menu = true; // Flag for showing main menu
+bool settings = false; // Flag for settings screen
+bool play_sound_once = true; // Flag for playing the sound only once
+const int WIDTH = 1000; // Window width
+const int HEIGHT = 600; // Window height
+const int BUTTON_HEIGHT = 50; // Button height
+const int BUTTON_WIDTH = 400; // Button width
+const int FPS = 60; // Frames per second
+const int LIVES = 1; // Initial number of lives
+Sound lost_heart; // Sound for lost heart
+Sound lost_game; // Sound for lost game
+Sound paddle_hard_hit; // Sound for hard paddle hit
+Sound paddle_hit; // Sound for regular paddle hit
+Texture2D Heart; // Heart image texture
+
+// Customizable Color constants
 Color Blue = Color{38, 154, 185, 255};
 Color Dark_Blue = Color{20, 133, 160, 255};
 Color Light_Blue = Color{129, 184, 204, 150};
 Color Yellow = Color{243, 213, 91, 255};
 
-
+/**
+ * Stats structure for storing game statistics.
+ */
 struct Stats {
-    int rally = 0;
-    int highest_rally = 0;
-    int player_score = 0;
-    int computer_score = 0;
-    int lives = 3;
-    float speed = 1.1;
+    int rally = 0;         // Current rally score
+    int highest_rally = 0; // Highest rally score
+    int player_score = 0;  // Player score
+    int computer_score = 0; // Computer score
+    int lives = LIVES;     // Number of lives
+    float speed = 1.1f;    // Ball speed multiplier
 };
 
 class Ball {
 public:
-    Vector2 position;
-    float speed_x;
-    float speed_y;
-    const float radius;
+    Vector2 position;  // Ball position
+    float speed_x;     // Speed in X direction
+    float speed_y;     // Speed in Y direction
+    const float radius; // Ball radius
 
+    /**
+     * Constructor for the Ball class.
+     *
+     * @param X Initial X position
+     * @param Y Initial Y position
+     * @param initialSpeedX Initial speed in X direction
+     * @param initialSpeedY Initial speed in Y direction
+     * @param ballRadius Ball radius
+     */
     Ball(const float & X, const float & Y, const float & initialSpeedX, const float & initialSpeedY, const float & ballRadius)
             : position({X, Y}), speed_x(initialSpeedX), speed_y(initialSpeedY), radius(ballRadius) {}
 
+    /** Moves the ball based on current speed. */
     void move() {
         position.x += speed_x;
         position.y += speed_y;
     }
 
+    /**
+     * Draws the ball to the screen.
+     * @param color Ball color
+     */
     void draw(const Color & color) const {
         DrawCircleV(position, radius, color);
     }
 
+    /**
+     * Resets the ball position and speed.
+     *
+     * @param X New X position
+     * @param Y New Y position
+     * @param newSpeedX New speed in X direction
+     * @param newSpeedY New speed in Y direction
+     */
     void reset(const float & X, const float &Y, const float & newSpeedX, const float & newSpeedY) {
         position = {X, Y};
         speed_x = newSpeedX;
@@ -59,17 +87,28 @@ public:
 
 class Paddle {
 public:
-    Vector2 position;
-    Vector2 size;
-    Color color;
+    Vector2 position; // Paddle position
+    Vector2 size;     // Paddle size
+    Color color;      // Paddle color
 
+    /**
+     * Constructor for Paddle class.
+     *
+     * @param X Initial X position
+     * @param Y Initial Y position
+     * @param width Paddle width
+     * @param height Paddle height
+     * @param color Paddle color
+     */
     Paddle(const float & X, const float & Y, const float & width, const float & height, const Color & color)
             : position({X, Y}), size({width, height}), color(color) {}
 
+    /** Draws the paddle to the screen. */
     void draw() const {
         DrawRectangleRounded(Rectangle{position.x, position.y, size.x, size.y}, 0.8, 0, color);
     }
 
+    /** Restricts the paddle's movement within the window bounds. */
     void limit_movement() {
         if (position.y + size.y >= HEIGHT){
             position.y = HEIGHT - size.y;
@@ -78,22 +117,49 @@ public:
         }
     }
 
+    /**
+     * Moves the paddle.
+     * 
+     * @param dy Delta Y value to move the paddle
+     */
     void move(const float & dy) {
         position.y += dy;
         limit_movement();
     }
 
+    /**
+     * Checks if the paddle collided with the ball.
+     * 
+     * @param ballPos Ball position
+     * @param ballRad Ball radius
+     * @return True if there is a collision, false otherwise
+     */
     bool collided(const Vector2 & ballPos, const float & ballRad){
         return CheckCollisionCircleRec(ballPos, ballRad, Rectangle {position.x, position.y, size.x, size.y});
     }
 };
 
-class CPU_Paddle: public Paddle{
+class CPU_Paddle: public Paddle {
 public:
+    /**
+     * Constructor for CPU_Paddle class.
+     * 
+     * @param X Initial X position
+     * @param Y Initial Y position
+     * @param width Paddle width
+     * @param height Paddle height
+     * @param color Paddle color
+     */
     CPU_Paddle(const float & X, const float & Y, const float & width, const float & height, const Color & color) : Paddle(X, Y, width, height, color) {}
 
-    void update(const Vector2 & ballPos, const float & ballSpeedY){
-        if (ballPos.x >= 650){
+    /**
+     * Updates CPU paddle's position based on the ball position and movement.
+     * 
+     * @param ballPos Ball position
+     * @param ballSpeedY Ball's vertical speed
+     */
+    void update(const Vector2 & ballPos, const float & ballSpeedY) {
+        if (ballPos.x >= 650){ // Follow ball only when it crosses a certain threshold
             if (position.y + size.y / 2 <= ballPos.y) {
                 position.y += std::abs(ballSpeedY);
             } else if (position.y + size.y / 2 > ballPos.y) {
@@ -110,19 +176,27 @@ public:
     }
 };
 
-
 class Button {
 private:
-    Rectangle bounds;
-    Color color;
-    Color textColor;
-    std::string text;
-    bool pressed;
+    Rectangle bounds; // Button bounds
+    Color color;      // Button background color
+    Color textColor;  // Button text color
+    std::string text; // Button text
+    bool pressed;     // Button press state
 
 public:
+    /**
+     * Constructor for Button class.
+     *
+     * @param bounds Button bounds as a Rectangle
+     * @param color Button color
+     * @param textColor Button text color
+     * @param text Text to display on the button
+     */
     Button(const Rectangle & bounds, const Color & color, const Color & textColor, const string & text)
             : bounds(bounds), color(color), textColor(textColor), text(text), pressed(false) {}
 
+    /** Draws the button to the screen. */
     void draw() {
         DrawRectangleRec(bounds, color);
         if (CheckCollisionPointRec(GetMousePosition(), bounds)) {
@@ -138,25 +212,38 @@ public:
         }
     }
 
+    /** Returns whether the button was pressed. */
     bool isPressed() const {return pressed;}
 
+    /** Resets the button press state. */
     void reset() {pressed = false;}
 };
 
-
 class TextBox {
 private:
-    Rectangle box;
-    char* text;
-    int maxChars;
-    int letterCount;
-    int framesCounter;
-    bool active;
+    Rectangle box;       // Text box bounds
+    char* text;          // Text in the text box
+    int maxChars;        // Max number of characters in text box
+    int letterCount;     // Current number of letters
+    int framesCounter;   // Frames counter for cursor blinking
+    bool active;         // Active state of text box
 
 public:
-    TextBox(const float & x, const float & y, const float & width, const float & height, const int & maxChars) : box({x, y, width, height}), maxChars(maxChars), letterCount(0), framesCounter(0), active(false)
-    {text = new char[maxChars + 1]();}
+    /**
+     * Constructor for TextBox class.
+     *
+     * @param x Initial X position
+     * @param y Initial Y position
+     * @param width Text box width
+     * @param height Text box height
+     * @param maxChars Maximum number of characters allowed
+     */
+    TextBox(const float & x, const float & y, const float & width, const float & height, const int & maxChars)
+        : box({x, y, width, height}), maxChars(maxChars), letterCount(0), framesCounter(0), active(false) {
+        text = new char[maxChars + 1]();
+    }
 
+    /** Updates the text box based on user input. */
     void Update() {
         if (CheckCollisionPointRec(GetMousePosition(), box)) {
             active = true;
@@ -184,6 +271,7 @@ public:
         }
     }
 
+    /** Draws the text box on screen. */
     void draw() const {
         DrawRectangleRec(box, active ? Light_Blue : Blue);
         DrawRectangleLinesEx(box, 1, active ? Yellow : Light_Blue);
@@ -194,25 +282,23 @@ public:
         }
     }
 
+    /** Returns the text currently in the text box. */
     const char* getText() const {return text;}
 
-    ~TextBox() {delete[] text;}
+    ~TextBox() {delete[] text;} // Destructor to clean up dynamically allocated text memory
 };
 
+// Instantiate game objects like Ball, Paddles, Stats, and Buttons
 Ball ball((float)WIDTH / 2, (float)HEIGHT / 2, 5, 5, 15);
 Paddle paddle(10.0, (float)HEIGHT / 2 - 50, 20.0, 100.0, WHITE);
 CPU_Paddle cpuPaddle(970.0, (float)HEIGHT / 2 - 50, 20.0, 100.0, WHITE);
 Stats stats;
-Button start_button({(float)(WIDTH - BUTTON_WIDTH) / 2, (float)(HEIGHT - BUTTON_HEIGHT) / 2, (float)BUTTON_WIDTH, (float)BUTTON_HEIGHT},
-                    Light_Blue, BLACK, "Start");
-Button settings_button({(float)(WIDTH - BUTTON_WIDTH) / 2, (float)(HEIGHT - BUTTON_HEIGHT) / 2 + 100, (float)BUTTON_WIDTH, (float)BUTTON_HEIGHT},
-                       Light_Blue, BLACK, "Settings");
-Button set_speed_button({(float)(WIDTH - BUTTON_WIDTH) / 2, (float)(HEIGHT - BUTTON_HEIGHT) / 2 + 100, (float)BUTTON_WIDTH, (float)BUTTON_HEIGHT},
-                       Light_Blue, RED, "Confirm");
-Button exit_settings({(float)(WIDTH - BUTTON_WIDTH) / 2, (float)(HEIGHT - BUTTON_HEIGHT) / 2 + 200, (float)BUTTON_WIDTH, (float)BUTTON_HEIGHT},
-                     Light_Blue, BLACK, "Exit");
+Button start_button({(float)(WIDTH - BUTTON_WIDTH) / 2, (float)(HEIGHT - BUTTON_HEIGHT) / 2, (float)BUTTON_WIDTH, (float)BUTTON_HEIGHT}, Light_Blue, BLACK, "Start");
+Button settings_button({(float)(WIDTH - BUTTON_WIDTH) / 2, (float)(HEIGHT - BUTTON_HEIGHT) / 2 + 100, (float)BUTTON_WIDTH, (float)BUTTON_HEIGHT}, Light_Blue, BLACK, "Settings");
+Button set_speed_button({(float)(WIDTH - BUTTON_WIDTH) / 2, (float)(HEIGHT - BUTTON_HEIGHT) / 2 + 100, (float)BUTTON_WIDTH, (float)BUTTON_HEIGHT}, Light_Blue, RED, "Confirm");
+Button exit_settings({(float)(WIDTH - BUTTON_WIDTH) / 2, (float)(HEIGHT - BUTTON_HEIGHT) / 2 + 200, (float)BUTTON_WIDTH, (float)BUTTON_HEIGHT}, Light_Blue, BLACK, "Exit");
 TextBox speed_box((float)(WIDTH - BUTTON_WIDTH) / 2, (float)(HEIGHT - BUTTON_HEIGHT) / 2, (float)BUTTON_WIDTH, (float)BUTTON_HEIGHT, 10);
-
+Button exit_end_screen({(float)(WIDTH - BUTTON_WIDTH) / 2, (float)(HEIGHT - BUTTON_HEIGHT) / 2 + 200, (float)BUTTON_WIDTH, (float)BUTTON_HEIGHT}, Light_Blue, BLACK, "Exit");
 
 #define UNBEATABLE_PONG_PONG_H
 
